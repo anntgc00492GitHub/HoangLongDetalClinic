a498dbc3d3a94c7f39d2d739ae5fbb51394b12fd
anntgc00492GitHub <anntgc00492@gmail.com>
2016-10-31 17:37:06 +0700

    Add project files.

---
+++ HoangLongDetalClinic.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HoangLongDetalClinic.Web.MvcApplication" Language="C#" %>
---
+++ HoangLongDetalClinic.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HoangLongDetalClinic.Web.Startup))]
+namespace HoangLongDetalClinic.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
